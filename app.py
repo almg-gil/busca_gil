7b827cfa1b3f843274bb4817a132d84f948b5427
almg-gil <gpgi.gdi@gmail.com>
2026-07-10 09:20:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ def conectar_banco():
         database=st.secrets["DB_NAME"],
         user=st.secrets["DB_USER"],
         password=st.secrets["DB_PASSWORD"],
-        port=st.secrets["DB_PORT"]
+        port=st.secrets["DB_PORT"],
+        sslmode="require"  # <- Esta linha é vital para a Direct Connection
     )
 
 st.title("🏛️ Silegis - Busca de Normas (Supabase + Streamlit)")
